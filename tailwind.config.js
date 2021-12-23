9e1cec7e3a6dbe201489d780d6d49e818ae42c30
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-23 19:26:46 +0300

    Configure theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const theme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   content: [
@@ -5,7 +7,25 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Open Sans"', ...theme.fontFamily.sans],
+        montserrat: ['"Montserrat"', ...theme.fontFamily.sans],
+      },
+      colors: {
+        'primary-light': '#f8f8f8',
+        'primary-dark': '#191919',
+        pinkish: {
+          100: '#edb5f5',
+          200: '#ed7ad4',
+          300: '#e86ed0',
+        },
+        blueish: {
+          100: '#e6f8f9',
+          200: '#b1e8ed',
+        },
+      },
+    },
   },
   plugins: [],
 }
