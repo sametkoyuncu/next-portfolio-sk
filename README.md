6eb9b9a72480bd838813663d1f5a9b008c8943a3
Samet Koyuncu <sametkoyuncu@live.com>
2021-12-26 20:42:03 +0300

    Fix class names

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Portfolyo
+
+Next.js ve Tailwindcss ile geliştirdiğim portfolyo sitem.
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- components/BlogCardHorizontal.js
+++ components/BlogCardHorizontal.js
@@ -7,35 +7,35 @@ function BlogCardHorizontal({
 }) {
   return (
     <motion.div
-      class="max-w-sm w-full lg:max-w-full lg:flex mb-5 shadow-lg "
+      className="max-w-sm w-full lg:max-w-full lg:flex mb-5 shadow-lg "
       style={{ cursor: 'pointer' }}
       whileHover={{ scale: 1.05, transition: { duration: 0.4 } }}
     >
       <div
-        class="h-48 lg:h-auto lg:w-48 flex-none bg-cover text-center overflow-hidden rounded-l-lg"
+        className="h-48 lg:h-auto lg:w-48 flex-none bg-cover text-center overflow-hidden rounded-l-lg"
         style={{ backgroundImage: `url('${cover.url}')` }}
         title="Woman holding a mug"
       ></div>
-      <div class=" p-4 flex flex-col justify-between leading-normal rounded-r-lg bg-white  dark:bg-zinc-800 ">
-        <div class="mb-8">
-          <div class="text-gray-900 dark:text-gray-100 font-bold text-xl mb-2">
+      <div className=" p-4 flex flex-col justify-between leading-normal rounded-r-lg bg-white  dark:bg-zinc-800 ">
+        <div className="mb-8">
+          <div className="text-gray-900 dark:text-gray-100 font-bold text-xl mb-2">
             {title}
           </div>
-          <p class="text-gray-700 dark:text-white text-base">
+          <p className="text-gray-700 dark:text-white text-base">
             {content.slice(0, 200) + '...'}
           </p>
         </div>
-        <div class="flex items-center">
+        <div className="flex items-center">
           <img
-            class="w-10 h-10 rounded-full mr-4"
+            className="w-10 h-10 rounded-full mr-4"
             src="https://avatars.githubusercontent.com/u/17934179?v=4"
             alt="Avatar of Samet Koyuncu"
           />
-          <div class="text-sm">
-            <p class="text-gray-900 leading-none dark:text-white">
+          <div className="text-sm">
+            <p className="text-gray-900 leading-none dark:text-white">
               Samet Koyuncu
             </p>
-            <p class="text-gray-600 dark:text-gray-300">
+            <p className="text-gray-600 dark:text-gray-300">
               {moment(createdAt).format('DD MMM yyyy')}
             </p>
           </div>
